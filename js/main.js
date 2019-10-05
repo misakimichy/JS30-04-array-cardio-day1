3012cf112ea1acfb9599e4313adab6398002a553
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-04 23:03:04 -0700

    Add filter method to show inventors born in 1500s.

--- js/main.js
+++ js/main.js
@@ -22,6 +22,10 @@
   
       // Array.prototype.filter()
       // 1. Filter the list of inventors for those who were born in the 1500's
+      const filter1500 = inventors.filter(inventor =>
+        (inventor.year >= 1500 && inventor.year < 1600)
+      );
+      console.table(filter1500);
   
       // Array.prototype.map()
       // 2. Give us an array of the inventors' first and last names
